582c620015c95d44ffae8fc398484a751c6540cd
Deepanshu <dee5073916@gmail.com>
2023-02-26 14:43:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './Appa.css';
+let name = "Deepanshu";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="container">
+      <h1>Hello {name}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, dicta natus. A doloribus provident at maxime deserunt ab non possimus quos numquam culpa eaque, mollitia accusamus. Vitae assumenda totam ex ut sed minima dolores praesentium, cum ad repellat sint nobis accusantium distinctio consequatur reprehenderit?</p></div>
+      <img src="" alt="" />
+    </>
+  
   );
 }
 
