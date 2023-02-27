cb94de381fe8c8a9ff3734a7b5cbbffb2eb3f9a4
Deepanshu <dee5073916@gmail.com>
2023-02-27 14:53:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
+// import { useState } from "react";
+import "./Appa.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+// import About from "./components/About";
+import React, { useState } from 'react';
+import Alert from './components/Alert';
 
-import './Appa.css';
-let name = "Deepanshu";
+
+// import { BrowserRouter as Router, Switch, Route, exact } from 'react-router-dom';
 
 
 function App() {
-  return (
-    
+ const [mode, setMode] = useState('light'); //Whether dark mode is enabled opr not
+ const [alert, setAlert] = useState(null);
+
+
+
+const showAlert = (messeage, type)=>{
+    setAlert({
+      msg: messeage,
+      type: type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    }, 1500);
+}
+
+ const  toggleMode=()=>{
+  if(mode === 'light'){
+    setMode('dark');
+    document.body.style.backgroundColor = 'grey';
+    showAlert("Dark mode has been enabled", "success")
+    document.title =' Icoder - Dark mode Enabled';
+    setInterval(() => {
+      document.title =' Icoder - is Awesome';
+    }, 2000);
+    setInterval(() => {
+      document.title =' Install Icoder now';
+    }, 1500);
+
+}else{
+  setMode('light');
+  document.body.style.backgroundColor = 'white'
+  showAlert("Light mode has been enabled", "success")
+  document.title =' Icoder - light mode Enabled'
+}
+}
+ return (
     <>
-      <nav>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </nav>
-      <div className="container">
-      <h1>Hello {name}</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, dicta natus. A doloribus provident at maxime deserunt ab non possimus quos numquam culpa eaque, mollitia accusamus. Vitae assumenda totam ex ut sed minima dolores praesentium, cum ad repellat sint nobis accusantium distinctio consequatur reprehenderit?</p></div>
-      <img src="" alt="" />
+     {/* <Router> */}
+     <Navbar title="Icoder  " mode={mode} toggleMode={ toggleMode }/>
+      <Alert alert={alert} />
+      <div className="container my-3">
+      {/* <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          */}
+          {/* <Route exact path="/"> */}
+          <TextForm showAlert={showAlert} heading="Enter to text to analyze below" mode={mode} />
+          {/* </Route>
+        </Switch> */}
+       
+     
+      </div>
+        {/* </Router> */}
     </>
-  
   );
 }
 
